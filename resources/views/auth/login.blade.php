32cc771410668828aeb0ca386d1a750e71b189fb
khanshehzad2001 <khanshehzad2001@gmail.com>
2025-05-21 23:44:19 +0300

    fxd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
             <div class="card card-login card-white">
                 <div class="card-header">
                     <img src="{{ asset('white') }}/img/card-primary.png" alt="">
-                    <h1 class="card-title">{{ _('Log in') }}</h1>
+                    <h1 class="card-title">{{ __('Log in') }}</h1>
                 </div>
                 <div class="card-body">
                     <div class="input-group{{ $errors->has('email') ? ' has-danger' : '' }}">
@@ -19,7 +19,7 @@
                                 <i class="tim-icons icon-email-85"></i>
                             </div>
                         </div>
-                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ _('Email') }}">
+                        <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{__('Email') }}">
                         @include('alerts.feedback', ['field' => 'email'])
                     </div>
                     <div class="input-group{{ $errors->has('password') ? ' has-danger' : '' }}">
@@ -28,18 +28,18 @@
                                 <i class="tim-icons icon-lock-circle"></i>
                             </div>
                         </div>
-                        <input type="password" placeholder="{{ _('Password') }}" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
+                        <input type="password" placeholder="{{ __('Password') }}" name="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}">
                         @include('alerts.feedback', ['field' => 'password'])
                     </div>
-                    <button type="submit" href="" class="btn btn-primary btn-block mb-3">{{ _('Log In') }}</button>
+                    <button type="submit" href="" class="btn btn-primary btn-block mb-3">{{ __('Log In') }}</button>
                     <div class="pull-left" hidden>
                         <h6>
-                            <a href="{{ route('register') }}" class="link footer-link">{{ _('Create Account') }}</a>
+                            <a href="{{ route('register') }}" class="link footer-link">{{ __('Create Account') }}</a>
                         </h6>
                     </div>
                     <div class="pull-right">
                         <h6>
-                            <a href="{{ route('password.request') }}" class="link footer-link">{{ _('Forgot password?') }}</a>
+                            <a href="{{ route('password.request') }}" class="link footer-link">{{ __('Forgot password?') }}</a>
                         </h6>
                     </div>
                 </div>
